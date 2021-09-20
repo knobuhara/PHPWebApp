c5dbc89b6a5e28ac5e1bc5c3412309015de648e1
KNobuhara <knobuhara@KNobuharanoMacBook-Pro.local>
2021-09-20 10:14:45 +0900

    20210920 bugfix in dbcon

--- dbcon.php
+++ dbcon.php
@@ -1,7 +1,7 @@
 <?php
 try {
-    $db = new PDO('mysql:dbname=nobuharaDB;charset=utf8mb4', 'nobuhara', '0911');
-#    $db = new PDO('mysql:dbname=bears_wp1;charset=utf8mb4', 'bears_wp1', 'kuma05taka12');
+#    $db = new PDO('mysql:dbname=nobuharaDB;charset=utf8mb4', 'nobuhara', '0911');
+    $db = new PDO('mysql:host=mysql6b.xserver.jp;dbname=bears_wp1;charset=utf8mb4', 'bears_wp1', 'kuma05taka12');
 }   catch (PDOException $e) {
     echo "データベース接続エラー　：".$e->getMessage();
 }
