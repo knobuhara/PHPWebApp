b1b282e5586f15f1f36291af245cc3631747a734
KNobuhara <knobuhara@KNobuharanoMacBook-Pro.local>
2021-09-23 11:02:20 +0900

    20210923 modified dbcon.php

--- dbcon.php
+++ dbcon.php
@@ -1,8 +1,12 @@
 <?php
-try {
-#    $db = new PDO('mysql:dbname=nobuharaDB;charset=utf8mb4', 'nobuhara', '0911');
-    $db = new PDO('mysql:host=mysql6b.xserver.jp;dbname=bears_wp1;charset=utf8mb4', 'bears_wp1', 'kuma05taka12');
-}   catch (PDOException $e) {
-    echo "データベース接続エラー　：".$e->getMessage();
-}
+
+    function dbConnect(){
+        try {
+            $db = new PDO('mysql:dbname=nobuharaDB;charset=utf8mb4', 'nobuhara', '0911');
+            return $db;
+        }   catch (PDOException $e) {
+            echo "データベース接続エラー　：".$e->getMessage();
+            exit();
+        }
+    } 
 ?>
