2c1a41142288eb071521e309d14093f8942ae45f
KNobuhara <knobuhara@KNobuharanoMacBook-Pro.local>
2021-09-25 20:48:56 +0900

    Bootstrap designfor  top page

--- index.php
+++ index.php
@@ -28,60 +28,74 @@ if (!empty($_POST)) {
         $_SESSION['join'] = $_POST;   // フォームの内容をセッションで保存
         header('Location: userconfirm.php');   // check.phpへ移動
         exit();
+    }else{
+        /*
+        echo <<<EOM
+        <script type="text/javascript">
+        alert("フォームを正しくご記入ください！");
+        </script>
+        EOM;
+        */
     }
 }
 ?>
+
 <!DOCTYPE html>
 <html lang="ja">
 <head>
-<meta charset="utf-8">
+    <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <!-- Bootstrap CSS -->
+    <!--
+    <link rel="stylesheet" href="css/style_bs.css"> 
+    -->
+
+    <link rel="stylesheet" href="css/style_bs.css"> 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU" crossorigin="anonymous">
     <link href="https://unpkg.com/sanitize.css" rel="stylesheet"/>
-    <link rel="stylesheet" href="css/style.css">
+    <link href="//netdna.bootstrapcdn.com/bootstrap/3.1.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+    <script src="//netdna.bootstrapcdn.com/bootstrap/3.1.0/js/bootstrap.min.js"></script>
+    <script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
+    <!--
+    <script src="./js/func.js"></script>
+    -->
+<!------ Include the above in your HEAD tag ---------->
     <title>アカウント作成</title>
 </head>
 <body>
-    <div class="content">
-        <form action="" method="POST">
-            <h1>宣原PHP登録システム　アカウント作成</h1>
-            <p>次のフォームに必要事項をご記入し、登録してください。</p>
-            <br>
- 
-            <div class="text-primary">
-                <label for="name">ユーザー名</label>
-                <input id="name" type="text" name="name">
+<div class="container">
+    <h1>宣原PHP登録システム　アカウント作成</h1>
+    <p>次のフォームに必要事項をご記入し、登録してください。</p>
+    <br>
+    <div class="row">
+        <div class="col-md-12">
+            <div class="wrap">
+                <p class="form-title">
+                    アカウント作成</p>
+                <form class="login" action="" method="POST">
+                <input type="text" name="name" placeholder="Username" />
+                <input type="email" name="email" placeholder="Email" />
+                <input type="password" name="password" placeholder="Password" />
+                <input type="submit" value="確認する" class="btn btn-success btn-sm" />
+                <br>
+                <br>
+                <div>
+                    <a href="./login.php">登録済みはこちらからログイン</a>
+                </div>
+                <div class="remember-forgot">
+                    <div class="row">
+                        <div class="col-md-6 forgot-pass-content">
+                            <a href="javascription:void(0)" class="forgot-pass">Forgot Password</a>
+                        </div>
+                    </div>
+                </div>
+                </form>
             </div>
- 
-            <div class="control">
-                <label for="email">メールアドレス<span class="required">必須</span></label>
-                <input id="email" type="email" name="email">
-                <?php if (!empty($error["email"]) && $error['email'] === 'blank'): ?>
-                    <p class="error">＊メールアドレスを入力してください</p>
-                <?php elseif (!empty($error["email"]) && $error['email'] === 'duplicate'): ?>
-                    <p class="error">＊このメールアドレスはすでに登録済みです</p>
-                <?php endif ?>
-            </div>
- 
-            <div class="control">
-                <label for="password">パスワード<span class="required">必須</span></label>
-                <input id="password" type="password" name="password">
-                <?php if (!empty($error["password"]) && $error['password'] === 'blank'): ?>
-                    <p class="error">＊パスワードを入力してください</p>
-                <?php endif ?>
-            </div>
-            <div class="control">
-                <button type="submit" class="pass-reset-submit btn btn-success btn-sm">確認する</button>
-            </div>
-            <a href="./login.php">登録済みはこちらからログイン</a>
-            <div>
-                
-            </div>
-        </form>
+        </div>
     </div>
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+</div>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-/bQdsTh/da6pkI1MST/rWKFNjaCP5gBSY4sEBT38Q/9RBh9AH40zEOg7Hlq2THRZ" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.3/dist/umd/popper.min.js" integrity="sha384-W8fXfP3gkOKtndU4JGtKDvXbO53Wy8SZCQHczT5FMiiqmQfUpWbYdTil/SxwZgAN" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/js/bootstrap.min.js" integrity="sha384-skAcpIdS7UcVUC05LJ9Dxay8AXcDYfBJqt1CJ85S/CFujBsIzCIv+l9liuYLaMQ/" crossorigin="anonymous"></script>
 </body>
 </html>
